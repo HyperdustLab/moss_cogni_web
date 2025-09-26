0995f1750dcf21eafea3ade3afcfe390d35b52cd
fangyeming <fangyemingzz@gmail.com>
2025-09-26 22:24:51 +0800

    Update Vite configuration to change API proxy target from localhost:9902 to 127.0.0.1:9999 for improved local development setup.

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5177,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:9902',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ''),
-    //   },
-    // },
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:9999',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
